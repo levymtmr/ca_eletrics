19280393d9d3c9aed13d50cccfb4c3701433adab
Levy Mota Moreira <levymmoreira@icloud.com>
2023-12-12 14:45:51 -0300

    Update main.py

--- estrela_delta/main.py
+++ estrela_delta/main.py
@@ -5,14 +5,6 @@ from operations import calculate_estrela_estrela, calculate_delta_estrela
 
 def calcular_valores(entries, config):
     try:
-        # Converter entradas para complexos
-        Van = complex(*map(float, entries['Van'].get().split(',')))
-        Vbn = complex(*map(float, entries['Vbn'].get().split(',')))
-        Vcn = complex(*map(float, entries['Vcn'].get().split(',')))
-        Zan = complex(*map(float, entries['Zan'].get().split(',')))
-        Zbn = complex(*map(float, entries['Zbn'].get().split(',')))
-        Zcn = complex(*map(float, entries['Zcn'].get().split(',')))
-
         # Cálculo diferenciado com base na configuração
         if config.get() == "Y-Y":
             return calculate_estrela_estrela(entries=entries)
@@ -41,24 +33,26 @@ root.title("Cálculos de Circuitos Trifásicos")
 
 # Criar campos de entrada
 entries = {}
-for label in ["Van", "Vbn", "Vcn", "Zan", "Zbn", "Zcn"]:
+for label, exemplo in [("Van", "(módulo, ângulo)"), ("Vbn", "(módulo, ângulo)"), ("Vcn", "(módulo, ângulo)"),
+                       ("Zan", "(real, imaginária)"), ("Zbn", "(real, imaginária)"), ("Zcn", "(real, imaginária)")]:
     frame = ttk.Frame(root)
     frame.pack(padx=10, pady=5, fill='x')
     ttk.Label(frame, text=label).pack(side='left')
     entry = ttk.Entry(frame)
     entry.pack(side='right', expand=True, fill='x')
-    entries[label] = entry
+    ttk.Label(frame, text=exemplo).pack(side='right')
+    entries[label] = entry  # Correção aqui
 
 # Adicionar botões de opção para a configuração do circuito
 config_var = tk.StringVar(value="Y-Y")  # Valor padrão
 ttk.Radiobutton(root, text="Estrela-Estrela",
                 variable=config_var, value="Y-Y").pack()
-ttk.Radiobutton(root, text="Delta-Estrela",
+ttk.Radiobutton(root, text="Estrela-Delta",
                 variable=config_var, value="Y-D").pack()
 
 # Botões para calcular e limpar
 results = {label: tk.StringVar() for label in [
-    "Ia", "Ib", "Ic", "Pa", "Pb", "Pc", "Pt", "Qa", "Qb", "Qc", "Qt", "PotApar","FP", "natureza"]}
+    "Ia", "Ib", "Ic", "Pa", "Pb", "Pc", "Pt", "Qa", "Qb", "Qc", "Qt", "Ps","FP", "natureza"]}
 ttk.Button(root, text="Calcular", command=lambda: atualizar_resultados(
     calcular_valores(entries, config_var), results)).pack(pady=5)
 ttk.Button(root, text="Limpar", command=lambda: limpar_campos(
